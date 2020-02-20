75ca0809a01a5e8954ac1206aa6330313f0b2fb8
Chakrit W <quaos.qrz@gmail.com>
2020-02-20 14:17:09 +0700

    Fix SQL statement format

--- csv-sql.sh
+++ csv-sql.sh
@@ -9,8 +9,8 @@ op="\`$op\`"
 columns=$(head -n 1 tmp.csv | sed 's/,/`,`/g' | tr -d "\r\n")
 columns="\`$columns\`"
 tail -n +2 tmp.csv | while read l ; do
-  values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
-  values="\"$values\""
+  values=$(echo $l | sed "s/,/\',\'/g" | tr -d "\r\n")
+  values="'$values'"
   echo "INSERT INTO $op($columns) VALUES ($values);"
 done > $opfile
 rm tmp.csv
