5d99bc5d7c51aeafcdd786c6ffc7021d8a6c3de2
Chakrit W <quaos.qrz@gmail.com>
2020-02-20 13:59:37 +0700

    Add Shebang; Modify to run on Mac OS

--- csv-sql.sh
+++ csv-sql.sh
@@ -1,13 +1,16 @@
+#!/bin/sh
+
 fname=$1
 sed 's/\s*,*\s*$//g' $fname > tmp.csv
-op=$(echo $fname | cut -d"." -f 1)
+op=$(echo $fname | sed -E 's/\.([^\.]+)$//')
+# cut -d"." -f 1)
 opfile="$op.sql"
 op="\`$op\`"
-columns=$(head --lines=1 tmp.csv | sed 's/,/`,`/g' | tr -d "\r\n")
+columns=$(head -n 1 tmp.csv | sed 's/,/`,`/g' | tr -d "\r\n")
 columns="\`$columns\`"
-tail --lines=+2 tmp.csv | while read l ; do
-values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
-values="\"$values\""
-echo "INSERT INTO $op($columns) VALUES ($values);"
+tail -n +2 tmp.csv | while read l ; do
+  values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
+  values="\"$values\""
+  echo "INSERT INTO $op($columns) VALUES ($values);"
 done > $opfile
 rm tmp.csv
